714db9c671b27b4578b3cb7af33434be0ba1c3b6
Miguel Villanueva <miguel.villanuevara@gmail.com>
2021-12-15 21:27:04 -0600

    asdas
    dsadsa

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,63 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
-import { ComentariosModel } from 'src/app/models/comentarios.model';
 
-// Model
-import { PostsModel } from 'src/app/models/posts.model';
-import { UsuarioModel } from 'src/app/models/usuarios.model';
-// import { ComentariosModel } from 'src/app/models/comentarios.model';
-
-// Service
-import { PostsService } from 'src/app/services/posts.service';
-import { UsuarioService } from 'src/app/services/usuarios.service';
-import { ComentariosService } from 'src/app/services/comentarios.service';
 @Component({
-  selector: 'app-post-display',
-  templateUrl: './posts.component.html'
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
 })
 export class PostsComponent implements OnInit {
 
-  posts: PostsModel[];
-  users: UsuarioModel[];
-  comments: ComentariosModel[]
+  constructor() { }
 
-  constructor(
-    private postsService: PostsService,
-    private usuarioService: UsuarioService,
-    private comentariosService: ComentariosService
-  ) {
-    this.getPosts()
-    this.getComments()
+  ngOnInit(): void {
   }
-
-  getPosts() {
-    this.postsService.getPosts()
-      .subscribe( posts => {this.posts = posts; this.setUserName()} )
-    this.usuarioService.getUsers()
-      .subscribe( users => {this.users = users; this.setUserName()})
-    console.log(this.posts)
-  }
-
-  setUserName() {
-    if (this.posts && this.users) {
-      for(const post of this.posts) {
-        for(const user of this.users) {
-          if (post.userId === user.id) {
-            post.name = user.name
-          }
-        }
-      }
-    }
-  }
-
-  getComments(): void {
-    this.comentariosService.getComments()
-      .subscribe(comments => this.comments = comments)
-  }
-
-  ngOnInit() {
-    // this.getComments()
-  }
-
 
 }
