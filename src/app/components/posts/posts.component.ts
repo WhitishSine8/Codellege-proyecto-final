ccbb643a1253d01f402b0b9aff445d3045bbac84
Miguel Villanueva <miguel.villanuevara@gmail.com>
2021-12-15 21:37:05 -0600

    aplicación estable

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,14 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+// Model
+import { UsuarioModel } from 'src/app/models/usuarios.model';
+import { PostsModel } from 'src/app/models/posts.model';
+
+// Service
+import { PostsService } from '../../services/posts.service';
+import { UsuarioService } from 'src/app/services/usuarios.service';
+
 
 @Component({
-  selector: 'app-posts',
+  selector: 'app-post-display',
   templateUrl: './posts.component.html',
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: PostsModel[];
+  users: UsuarioModel[];
 
-  ngOnInit(): void {
+  constructor(
+    private postsService: PostsService,
+    private usuarioService: UsuarioService
+  ) {
+    this.getPosts()
   }
+
+  getPosts() {
+    this.postsService.getPosts()
+      .subscribe( posts => {this.posts = posts; this.setUserName()} )
+    this.usuarioService.getUsers()
+      .subscribe( users => {this.users = users; this.setUserName()})
+    console.log(this.posts)
+  }
+
+  setUserName() {
+    if (this.posts && this.users) {
+      for(const post of this.posts) {
+        for(const user of this.users) {
+          if (post.userId === user.id) {
+            post.name = user.name
+          }
+        }
+      }
+    }
+  }
+
+  ngOnInit() { }
 
 }
